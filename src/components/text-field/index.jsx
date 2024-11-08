3ab4d53b1c89a0eb2b0cb43851df08bba0343a9c
SanuRaj-K <sanusrk29@gmail.com>
2024-11-08 15:16:09 +0530

    update:updating UI

--- src/components/text-field/index.jsx
+++ src/components/text-field/index.jsx
@@ -5,13 +5,13 @@ export default function TextFields({ onRemove }) {
   return (
     <div className="border-2 h-[45px] bg-[#f5f8fb] mb-1 rounded-md text-[10px] border-[#e2e8f0] flex justify-between items-center w-full">
       <input
-        className="px-2 h-[45px] outline-blue-600 border-r-2 bg-[#f5f8fb] w-1/2"
+        className="px-2 h-[40px]  outline-blue-600 border-r-2 bg-[#f5f8fb] w-1/2"
         placeholder="Name"
         type="text"
       />
       <div className="flex items-center group w-1/2">
         <input
-          className="px-2 h-[45px] w-full outline-blue-600 bg-[#f5f8fb]"
+          className="px-2 h-[40px] w-full outline-blue-600 bg-[#f5f8fb]"
           placeholder="Value"
           type="text"
         />
